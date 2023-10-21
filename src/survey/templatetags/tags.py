80d91db98e4d436e8398c54b9946198bbf77d287
M Hadi Sasmito <keselyoleren@gmail.com>
2023-10-21 21:06:45 +0700

    filter date rage

--- src/survey/templatetags/tags.py
+++ src/survey/templatetags/tags.py
@@ -22,7 +22,10 @@ def survey_result(question, customer):
 @register.filter(name='avg_survey')
 def avg_survey(question):
     average_result = SurveyResult.objects.filter(question_id=question, responden__user__ptm_location=get_user().ptm_location).aggregate(avg_resp_int=Avg('resp_int'))
-    return "{:.1f}".format(average_result['avg_resp_int'])
+    try:
+        return "{:.1f}".format(average_result['avg_resp_int'])
+    except:
+        return average_result['avg_resp_int']
     
 
 
